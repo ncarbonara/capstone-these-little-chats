28c4ac4847b4bd24933460efed24828dbf14f211
Unknown <ncarbonara235@gmail.com>
2019-03-02 23:55:02 -0500

    Sharpened Text and Added Quit Button
    Title says all.

---
+++ These Little Chats/Assets/Scripts/QuitScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuitScript : MonoBehaviour {
+
+    /// <summary>
+    /// Quits the game.
+    /// </summary>
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
