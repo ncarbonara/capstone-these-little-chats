b8e188bec17be5d0abdcc417c11f3c6ee3803c0b
Unknown <ncarbonara235@gmail.com>
2019-03-16 13:54:49 -0400

    Finished Adding First Pass of Audio Functionality
    Game should now play sound FX and music on command from a Yarn file.

---
+++ These Little Chats/Assets/Scripts/MusicYarnCommands.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.UI;   //Let's us talk to UI stuff.
+using Yarn.Unity;   //Lets us talk to Yarn stuff.
+
+/// <summary>
+/// Contains all Yarn commands associated with music that are called from a Yarn file
+/// </summary>
+public class MusicYarnCommands : MonoBehaviour {
+
+    public AudioClip musicTrackOne;
+    public AudioClip musicTrackTwo;
+    public AudioClip musicTrackThree;
+    public AudioClip musicTrackFour;
+
+    [YarnCommand("triggerSound")]
+    public void TriggerSound(string sound)
+    {
+        if(sound == "musicTrackOne")
+        {
+            this.GetComponent<AudioSource>().clip = musicTrackOne;
+            this.GetComponent<AudioSource>().Play();
+        }
+        else if(sound == "musicTrackTwo")
+        {
+            this.GetComponent<AudioSource>().clip = musicTrackTwo;
+            this.GetComponent<AudioSource>().Play();
+        }
+        else if(sound == "musicTrackThree")
+        {
+            this.GetComponent<AudioSource>().clip = musicTrackThree;
+            this.GetComponent<AudioSource>().Play();
+        }
+        else if(sound == "musicTrackFour")
+        {
+            this.GetComponent<AudioSource>().clip = musicTrackFour;
+            this.GetComponent<AudioSource>().Play();
+        }
+    }
+}
---
+++ These Little Chats/Assets/Scripts/SoundFXYarnCommands.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.UI;   //Let's us talk to UI stuff.
+using Yarn.Unity;   //Lets us talk to Yarn stuff.
+
+/// <summary>
+/// Contains all Yarn commands associated with SFX that are called from a Yarn file
+/// </summary>
+public class SoundFXYarnCommands : MonoBehaviour {
+
+    public AudioClip doorKnock;
+    public AudioClip placeDMScreen;
+    public AudioClip doorOpenClose;
+    public AudioClip sitDown;
+
+    [YarnCommand("triggerSound")]
+    public void TriggerSound(string sound)
+    {
+        if(sound == "doorKnock")
+        {
+            this.GetComponent<AudioSource>().clip = doorKnock;
+            this.GetComponent<AudioSource>().Play();
+        }
+        else if(sound == "placeDMScreen")
+        {
+            this.GetComponent<AudioSource>().clip = placeDMScreen;
+            this.GetComponent<AudioSource>().Play();
+        }
+        else if(sound == "doorOpenClose")
+        {
+            this.GetComponent<AudioSource>().clip = doorOpenClose;
+            this.GetComponent<AudioSource>().Play();
+        }
+        else if(sound == "sitDown")
+        {
+            this.GetComponent<AudioSource>().clip = sitDown;
+            this.GetComponent<AudioSource>().Play();
+        }
+    }
+}
